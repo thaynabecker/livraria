4010c432f2ad64f03442f4f1c02e2efddcdd8821
thaynabecker <thaynabecker.ifcaraquari@gmail.com>
2025-05-09 11:06:25 -0300

    FEAT: Criação da API para Livro

--- livraria/views.py
+++ livraria/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria, Editora, Autor
-from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer
+from livraria.models import Categoria, Editora, Autor, Livro
+from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroListSerializer, LivroDetailSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
@@ -14,3 +14,13 @@ class EditoraViewSet(ModelViewSet):
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return LivroListSerializer
+        elif self.action == "retrieve":
+            return LivroDetailSerializer
+        return LivroSerializer
