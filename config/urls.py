a5be926dfc443335ca2b25e906875a46e15388c3
thaynabecker <thaynabecker.ifcaraquari@gmail.com>
2025-05-09 10:55:41 -0300

    FEAT: Criação da API para Autor

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,16 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet
+
+router = DefaultRouter()
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"autores", AutorViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from livraria.models import Categoria, Editora, Autor
+from livraria.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
